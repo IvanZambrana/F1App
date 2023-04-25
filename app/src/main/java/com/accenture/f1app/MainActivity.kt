b2264f5fe8ce392537064d4da8104a6d0aaedc6d
ivan_zambrana <ivanzambrananaranjo@gmail.com>
2023-04-25 14:37:10 +0200

    Fragments and navBar

--- app/src/main/java/com/accenture/f1app/MainActivity.kt
+++ app/src/main/java/com/accenture/f1app/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.accenture.f1app
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.accenture.f1app.databinding.ActivityMainBinding
+import com.accenture.f1app.fragments.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Set home fragment as default
+        makeCurrentFragment(HomeFragment())
+
+        //Replace Fragment with the bottom nav bar
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.itemHome -> makeCurrentFragment(HomeFragment())
+                R.id.itemDrivers -> makeCurrentFragment(DriversFragment())
+                R.id.itemCircuits -> makeCurrentFragment(CircuitsFragment())
+                R.id.itemTeams -> makeCurrentFragment(TeamsFragment())
+                R.id.itemFav -> makeCurrentFragment(FavouritesFragment())
+            }
+            true
+        }
+
+    }
+
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fl_wrapper, fragment)
+        fragmentTransaction.commit()
     }
 }
