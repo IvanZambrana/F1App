8c352b06ba4da127a8b1f37ab92c2a4abcb04a40
ivan_zambrana <ivanzambrananaranjo@gmail.com>
2023-05-08 11:58:50 +0200

    SplashScreen and final details

---
+++ app/src/main/java/com/accenture/f1app/ui/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.accenture.f1app.ui.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
